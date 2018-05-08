7bf16d901c0dee48f4a9b854a282044d2b089be0
Anderson <Anderson@192.168.0.182>
2018-05-07 21:23:36 -0300

    Adição de log de inicialização

--- src/main/java/br/com/baratella/Application.java
+++ src/main/java/br/com/baratella/Application.java
@@ -3,9 +3,13 @@
  */
 package br.com.baratella;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import br.com.baratella.util.InformacoesAplicacaoSingleton;
 
 /**
  * The Class Application.
@@ -13,6 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application {
+	
 
 	/**
 	 * Método main para execução do Spring Boot.
@@ -20,6 +25,8 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(Application.class);
+		logger.info("A aplicação foi iniciada: " + InformacoesAplicacaoSingleton.INSTANCE.getDataInicializacao());
 		SpringApplication.run(Application.class, args);
 	}
 }
