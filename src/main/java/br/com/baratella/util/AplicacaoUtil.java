cb0e97a2471fecbaba1bcbb2eb62333256545a5d
Anderson <Anderson@192.168.0.182>
2018-05-07 21:23:15 -0300

    Comentário de loop

--- src/main/java/br/com/baratella/util/AplicacaoUtil.java
+++ src/main/java/br/com/baratella/util/AplicacaoUtil.java
@@ -17,6 +17,7 @@ public class AplicacaoUtil {
 	 * @return
 	 */
 	public static boolean validaCamposObrigatorios(Object... objects) {
+		//Itera sobre uma coleção de objetos verificando se todos os itens estão valorados
 		for (Object object : objects) {
 			if (ObjectUtils.isEmpty(object)) {
 				return false;
